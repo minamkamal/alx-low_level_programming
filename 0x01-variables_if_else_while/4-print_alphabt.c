4cfc81d3798400d3b8c639f165716f19a652ab55
mina <mina.m.kamal@gmail.com>
2023-06-16 14:29:58 -0700

    4-print_alphabt.c

--- 0x01-variables_if_else_while/4-print_alphabt.c
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -12,16 +12,16 @@
 
 int main(void)
 {
-	char low, e, q;
+        char i, e, q;
 
-	e = 'e';
-	q = 'q';
+        e = 'e';
+        q = 'q';
 
-	for (low = 'a'; low <= 'z'; low++)
-	{
-	if (low != e && low != q)
-	putchar(low);
-	}
-	putchar('\n');
-	return (0);
+        for (i = 'a'; i <= 'z'; i++)
+        {
+        if (i != e && i != q)
+        putchar(i);
+        }
+        putchar('\n');
+        return (0);
 }
