53aede52cb2eb3c393d164b766792ebcd26a91f9
mina <mina.m.kamal@gmail.com>
2023-06-16 12:29:37 -0700

    3-print_alphabets.c

---
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -0,0 +1,23 @@
+#include <stdlib.h>
+#include <time.h>
+/* more headers goes there */
+#include <stdio.h>
+/**
+ * main - Prints the alphabet.
+ *
+ * Return: Always 0 (Success)
+ */
+
+/* betty style doc for function main goes there */
+int main(void)
+{
+        char alp[52] = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ$";
+        int i;
+
+        for (i = 0; i < 52; i++)
+        {
+                putchar(alp[i]);
+        }
+        putchar('\n');
+        return (0);
+}
