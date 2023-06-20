6acca1c8822c8391bed6f68b0002187bf1630c03
mina <mina.m.kamal@gmail.com>
2023-06-20 12:57:07 -0700

    #include main.h

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,14 @@
+#include "main.h"
+
+/**
+ * print alphabet - print in lower
+ * Return 0 
+ */
+void print_alphabet(void)
+{
+	char alpha;
+	for (alpha ='a'; alpha <='z'; alpha++)
+		_putchar(alpha);
+	_putchar('\n');
+
+}
