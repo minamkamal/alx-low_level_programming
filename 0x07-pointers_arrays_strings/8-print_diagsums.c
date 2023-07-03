c32276278ce1c65f6276370b78c31c7717e15f10
mina <mina.m.kamal@gmail.com>
2023-07-03 20:04:16 +0300

    8-print_diagsums.c

--- 0x07-pointers_arrays_strings/8-print_diagsums.c
+++ 0x07-pointers_arrays_strings/8-print_diagsums.c
@@ -1,11 +1,13 @@
-i#include "main.h"
+#include "main.h"
 #include <stdio.h>
+
 /**
  * print_diagsums - Entry point
  * @a: input
  * @size: input
  * Return: Always 0 (Success)
  */
+
 void print_diagsums(int *a, int size)
 {
 	int sum1, sum2, y;
